59d9a825f2ec1305d8d37fc3f8fc9332a7da16fe
Drian Naude <drian@partly.com>
2022-05-21 12:57:09 +1200

    feat: 🎸 wallet-lookup
    wip: wallet info lookup

--- packages/ui/src/pages/Dashboard/DashboardPage.tsx
+++ packages/ui/src/pages/Dashboard/DashboardPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BlockNumberStatus } from '../../components/BlockNumberStatus';
+import { WalletInfo } from '../../components/WalletInfo';
 
 const DashboardPageComponent = () => {
   return (
-    <div className="row">
-      <div className="col-8"></div>
-      <div className="col-4">
-        <BlockNumberStatus />
+    <div className="container">
+      <div className="row">
+        <div className="col-8"></div>
+        <div className="col-4">
+          <BlockNumberStatus />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <WalletInfo />
+        </div>
       </div>
     </div>
   );
